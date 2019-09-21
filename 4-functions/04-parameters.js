efd751e61dc3ebb10865f3a0faf02b6663288790
wchestnuti27 <wchestnuti27@users.noreply.github.com>
2019-09-21 18:06:07 -0400

    functionsAndParameters

--- 4-functions/04-parameters.js
+++ 4-functions/04-parameters.js
@@ -1,10 +1,30 @@
-function zach (ticket) {  
+function zach(ticket) {
     console.log(`I exchanged my ticket for a ${ticket}`);
-
 }
-zach('cheese pizza')
-
-// function parameters are place holders for data thatwe pass into the function when calling or 'invoking' the function.
-
-// Challenge
-
+​
+zach('cheese pizza');
+​
+// function parameters are place holders for data that we pass into the function when calling or 'invoking' the function
+​
+// functions need parameters to be able to accept data that we pass to them
+​
+/*
+CHALLENGE
+************
+​
+    - Write a function that takes two parameters:
+        - one parameter is for a first name,
+        - the other parameter is for a last name;
+        - have them come together in a variable inside the function.
+        - console.log 'Hello, my name is <your name>'
+        - call (or invoke) your function
+*/
+​
+function myName(fName, lName) {
+    let fullName = `${fName} ${lName}`;
+    console.log(`Hello, my name is ${fullName}`)
+}
+​
+myName('Zach', 'Maynard');
+myName('Jessica', 'Cooke');
+myName('Daniel', 'Lufcy');
