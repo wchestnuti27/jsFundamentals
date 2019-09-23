c2c45886d45d0414dabb5b9315007145b43eed53
wchestnuti27 <wchestnuti27@users.noreply.github.com>
2019-09-23 16:26:30 -0400

    portexample

---
+++ morningChallenges/02.01.js
@@ -0,0 +1,18 @@
+// let sampleString = 'pig latin' 
+
+function pigLatin(sampleString);
+// splitting our string by word. remember that the split method returns an array. We are storing tbat array in a new variable called wordsArray ---> ('pig', 'latin')
+let wordsArray = str.split(' ')
+console.log(wordsArray);
+
+wordsArray.forEach((word, index)) => {
+console.log(word);
+let lettersArray = word.split('')
+console.log(lettersArray); !== 'a' 
+
+while (lettersArray[0] 
+
+return wordsArray. joint(' ');
+
+
+
---
+++ morningChallenges/rep'lChallenge.js
@@ -0,0 +1,18 @@
+let Gladiator = {
+    cast: {
+        Maximus: 'Russell Crowe', 'main', 54
+        Commodus: 'Joaquin Phoenix', 'brother', 45
+        Lucilla: 'Connie Nielsen', 'sister', 54
+        Proximo: 'Oliver Reed', 'Slave Owner', 81
+        Marcus Aurelius: 'Ceasar','killed', 72
+        Gracchus: 'Derek Jacobi', 'Senator', 81
+        Juba: 'Djimon Hounsou', 'slave', 55
+        Quintus: 'Tomas Arana', 'soldier', 64
+    }
+    runTime: { 
+        2Hrs 35Mins
+    }
+    Genre: {
+        'Action'
+    }
+}
